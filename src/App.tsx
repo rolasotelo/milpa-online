fba363213980a2eb6423a319702755e28318444b
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-06-19 20:52:54 -0500

    ⚙️  CONFIGURATION: Add tailwind config file

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ class App extends React.Component<Props> {
     const { name } = this.props;
     return (
       <>
-        <h1 className="text-4xl text-white bg-black">Hola {name}</h1>
+        <h1 className="text-4xl text-white bg-black hover:bg-gray-700">
+          Hola {name}
+        </h1>
       </>
     );
   }
