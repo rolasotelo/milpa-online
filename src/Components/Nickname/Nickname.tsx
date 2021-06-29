877d7737382ccb1c0351c0d23202c56949d75bb9
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-06-28 23:56:51 -0500

    📦 NEW: Create Nickname component

---
+++ src/Components/Nickname/Nickname.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {}
+
+const Nickname = (props: Props) => {
+  return (
+    <div>
+      <h1 className="text-4xl text-black">MIROSLAVA</h1>
+      <a href="/play">A play</a>
+    </div>
+  );
+};
+
+export default Nickname;
--- src/Containers/WelcomePage/WelcomePage.tsx
+++ src/Containers/WelcomePage/WelcomePage.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
 import Layout from "../../Components/Layout/Layout";
+import Nickname from "../../Components/Nickname/Nickname";
 
 interface Props {}
 
 const WelcomePage = (props: Props) => {
   return (
     <Layout>
-      <h1 className="text-4xl text-black">MILPA</h1>
-      <a href="/play">A play</a>
+      <Nickname />
     </Layout>
   );
 };
