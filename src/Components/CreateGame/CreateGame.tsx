ca562d94b50e130671acaed63625a5d7c00e5758
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-06-30 21:28:04 -0500

    🎀 STYLING: Set backgrounds for create and join game boxes

--- src/Components/CreateGame/CreateGame.tsx
+++ src/Components/CreateGame/CreateGame.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import CreateGameImage from "../../../static/create-game.png";
 
 interface Props {}
 
 const CreateGame = (props: Props) => {
-  return <div className="bg-mexicanGreen w-80 h-96">Create Game</div>;
+  return (
+    <div
+      className="w-80 h-96 bg-contain bg-no-repeat"
+      style={{ backgroundImage: `url(${CreateGameImage})` }}
+    >
+      Create Game
+    </div>
+  );
 };
 
 export default CreateGame;
--- src/Components/JoinGame/JoinGame.tsx
+++ src/Components/JoinGame/JoinGame.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import JoinGameImage from "../../../static/join-game.png";
 
 interface Props {}
 
 const JoinGame = (props: Props) => {
-  return <div className="bg-mexicanGreen-dark w-80 h-96">Join Game</div>;
+  return (
+    <div
+      className="w-80 h-96 bg-contain bg-no-repeat"
+      style={{ backgroundImage: `url(${JoinGameImage})` }}
+    >
+      Join Game
+    </div>
+  );
 };
 
 export default JoinGame;
