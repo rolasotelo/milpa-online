a1abb8ae545b2d5d09994e6cfe9e76b4226d4ead
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-07-01 13:57:53 -0500

    ⚙️  CONFIGURATION: Add test: watch script and fileTransformer for static files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  transform: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/fileTransformer.js",
+  },
 };
