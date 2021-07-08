4d4f639194608a7ffd78174bf458d19d61abf118
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-07-07 21:57:25 -0500

    📦 NEW: Add LayoutGame container

---
+++ src/Components/LayoutGame/LayoutGame.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import logo from "./../../../static/logo.png";
+
+interface Props {
+  children: ReactNode;
+}
+
+const LayoutGame = (props: Props) => {
+  return (
+    <div className="bg-mexicanBone min-h-screen">
+      <div className="bg-mexicanBlue">
+        <div className="container mx-auto p-2">
+          <nav className="flex justify-between text-mexicanPink">
+            <div className="self-center">
+              <a href="#">Menu</a>
+            </div>
+            <div>
+              <img src={logo} className="h-10" />
+            </div>
+            <div className="self-center">
+              <a href="#">Rules</a>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <div className="max-w-6xl flex flex-col justify-center py-5 mx-auto">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default LayoutGame;
--- src/Containers/Game/Game.tsx
+++ src/Containers/Game/Game.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import LayoutGame from "../../Components/LayoutGame/LayoutGame";
 import useGameContext from "../../Hooks/useGameContext/useGameContext";
 
 interface Props {}
 
 const Game = (props: Props) => {
   const context = useGameContext();
-  return <div>Milpa Game</div>;
+  return <LayoutGame>Game</LayoutGame>;
 };
 
 export default Game;
