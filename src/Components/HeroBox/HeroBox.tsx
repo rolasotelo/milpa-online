1eafedc6e145f6fc832f564eb253dfba9e25cb1c
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-07-21 22:57:36 -0500

    📦 NEW: Add Herobox to home page

--- src/Components/HeroBox/HeroBox.tsx
+++ src/Components/HeroBox/HeroBox.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import HeroBoxImage from "../../../static/hero-box.png";
 
 interface Props {}
 
 const HeroBox = (props: Props) => {
-  return <div></div>;
+  return (
+    <div
+      className="w-80 h-96 bg-contain bg-no-repeat"
+      style={{ backgroundImage: `url(${HeroBoxImage})` }}
+    ></div>
+  );
 };
 
 export default HeroBox;
--- src/Containers/WelcomePage/WelcomePage.tsx
+++ src/Containers/WelcomePage/WelcomePage.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
+import HeroBox from "../../Components/HeroBox/HeroBox";
 import Layout from "../../Components/Layout/Layout";
 
 interface Props {}
 
 const WelcomePage = (props: Props) => {
-  return <Layout>Bienvenido a Milpa</Layout>;
+  return (
+    <Layout>
+      <HeroBox />{" "}
+    </Layout>
+  );
 };
 
 export default hot(WelcomePage);
