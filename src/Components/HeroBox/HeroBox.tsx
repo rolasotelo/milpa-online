5dbf9c02b38545e8ea9049ffb95171db57c0aaa1
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-08-23 23:07:27 -0500

    🎀 STYLING: Add Goodlife font in play button

--- src/Components/HeroBox/HeroBox.tsx
+++ src/Components/HeroBox/HeroBox.tsx
@@ -13,6 +13,12 @@ const HeroBox = (props: Props) => {
           <button
             className="bg-button-play w-52 h-24 px-8 mt-32 md:mt-56 focus:outline-none"
             onClick={props.onClick}
+            style={{
+              fontFamily: "goodlife-sans-condensed, sans-serif",
+              fontWeight: 400,
+              fontStyle: "normal",
+              fontSize: "3rem",
+            }}
           >
             PLAY
           </button>
