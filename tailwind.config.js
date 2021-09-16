a382660ed2cdc67351a7ffee184060950148bb66
Rolando Sotelo <rola@hey.com>
2021-09-15 22:46:19 -0500

    🎀 STYLING: create new sprites and add them to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -105,10 +105,21 @@ module.exports = {
       "button-logo": "url('/static/buttons.svg#logo-button-usage')",
       "button-logo-pressed":
         "url('/static/buttons.svg#logo-button-pressed-usage')",
-      "button-play": "url('/static/buttons.svg#button-play-usage')",
-      herobox: "url('/static/herobox.svg#herobox-usage')",
-      "milpa-name-web": "url('/static/herobox.svg#milpa-name-web-usage')",
-      "milpa-name-mobile": "url('/static/herobox.svg#milpa-name-mobile-usage')",
+      "button-blue": "url('/static/buttons.svg#blue-button-usage')",
+      "button-blue-pressed":
+        "url('/static/buttons.svg#blue-button-pressed-usage')",
+      "button-pink": "url('/static/buttons.svg#pink-button-usage')",
+      "button-pink-pressed":
+        "url('/static/buttons.svg#pink-button-pressed-usage')",
+      "play-background": "url('/static/welcome.svg#play-background-usage')",
+      "milpa-cardgame": "url('/static/welcome.svg#milpa-cardgame-usage')",
+      "milpa-cardgame-mobile":
+        "url('/static/welcome.svg#milpa-cardgame-mobile-usage')",
+      "create-background": "url('/static/create.svg#create-background-usage')",
+      "join-background": "url('/static/create.svg#join-background-usage')",
+      "green-banner": "url('/static/banners.svg#green-banner-usage')",
+      "green-banner-mobile":
+        "url('/static/banners.svg#green-banner-mobile-usage')",
     },
     backgroundOpacity: (theme) => theme("opacity"),
     backgroundPosition: {
