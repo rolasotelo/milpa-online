009e9c5840d0ac8030a3a8078db25314b4cc73ad
Rolando Sotelo <rola@hey.com>
2021-11-17 20:27:26 -0600

    ⚙️  CONFIGURATION: Set up i18n for languages

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./styles.css";
+import "./i18n";
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App />, mountNode);
