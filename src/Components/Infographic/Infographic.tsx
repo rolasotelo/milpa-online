82fbb5f336e59ad884192fd223b407f98a63c5a9
Rolando Sotelo <rola@hey.com>
2021-11-30 02:17:00 -0600

    🎀 STYLING: brush strokes in infographic

--- src/Components/Infographic/Infographic.tsx
+++ src/Components/Infographic/Infographic.tsx
@@ -16,49 +16,53 @@ const lngs: Languages = {
 const Infographic = (props: Props) => {
   const { t, i18n } = useTranslation();
   return (
-    <div className="relative flex-col w-screen">
-      <div className="relative w-20.38rem mx-auto max-w-100vw md:w-herobox-web h-herobox-web bg-milpaBlue-default ring-8 ring-offset-0 ring-milpaBlue-dark ">
-        <div className="flex flex-row  h-full">
-          <div className="flex flex-col w-72 items-center">
-            <div
-              className="text-mexicanBone w-48 h-60"
-              style={{
-                fontFamily: "goodlife-sans-condensed, sans-serif",
-                fontWeight: 400,
-                fontStyle: "normal",
-                fontSize: "3.375rem",
-              }}
-            >
-              {t("welcome.infographic.whatisamilpa")}
-            </div>
+    <div className="relative flex-col w-screen overflow-x-hidden">
+      <div className="relative w-20.38rem mx-auto max-w-100vw md:w-herobox-web h-herobox-web mt-5 md:mt-herobox-top-web">
+        <div className="absolute -inset-y-14 -left-44 h-brush-pink-3 w-brush-pink-3 bg-brush-pink-3" />
+        <div className="absolute -inset-y-52 -right-44 h-brush-pink-4 w-brush-pink-4 bg-brush-pink-4" />
+        <div className="relative w-20.38rem mx-auto max-w-100vw md:w-herobox-web h-herobox-web bg-milpaBlue-default ring-8 ring-inset ring-milpaBlue-dark ">
+          <div className="flex flex-row  h-full">
+            <div className="flex flex-col w-72 items-center">
+              <div
+                className="text-mexicanBone w-48 h-60"
+                style={{
+                  fontFamily: "goodlife-sans-condensed, sans-serif",
+                  fontWeight: 400,
+                  fontStyle: "normal",
+                  fontSize: "3.375rem",
+                }}
+              >
+                {t("welcome.infographic.whatisamilpa")}
+              </div>
 
-            <div className="flex flex-col w-48">
-              {Object.keys(lngs).map((lng) => (
-                <button
-                  key={lng}
-                  className={`${
-                    i18n.resolvedLanguage === lng
-                      ? "text-mexicanGreen-light underline"
-                      : "text-mexicanBone"
-                  } my-1 text-left`}
-                  style={{
-                    fontWeight: i18n.resolvedLanguage === lng ? 700 : 400,
-                    fontFamily: "goodlife-sans-condensed, sans-serif",
-                    fontStyle: "normal",
-                    fontSize: "1.5rem",
-                  }}
-                  type="submit"
-                  onClick={() => i18n.changeLanguage(lng)}
-                >
-                  {`${lngs[lng].nativeName}`}
-                  &#160;
-                </button>
-              ))}
+              <div className="flex flex-col w-48">
+                {Object.keys(lngs).map((lng) => (
+                  <button
+                    key={lng}
+                    className={`${
+                      i18n.resolvedLanguage === lng
+                        ? "text-mexicanGreen-light underline"
+                        : "text-mexicanBone"
+                    } my-1 text-left`}
+                    style={{
+                      fontWeight: i18n.resolvedLanguage === lng ? 700 : 400,
+                      fontFamily: "goodlife-sans-condensed, sans-serif",
+                      fontStyle: "normal",
+                      fontSize: "1.5rem",
+                    }}
+                    type="submit"
+                    onClick={() => i18n.changeLanguage(lng)}
+                  >
+                    {`${lngs[lng].nativeName}`}
+                    &#160;
+                  </button>
+                ))}
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col w-full h-full">
-            <div className="col-span-5 w-full"></div>
-            <div className="row-span-2 col-span-4"></div>
+            <div className="flex flex-col w-full h-full">
+              <div className="col-span-5 w-full"></div>
+              <div className="row-span-2 col-span-4"></div>
+            </div>
           </div>
         </div>
       </div>
--- src/Containers/Welcome/index.tsx
+++ src/Containers/Welcome/index.tsx
@@ -22,11 +22,9 @@ export const Welcome = (props: Props) => {
         <div className="absolute inset-x-0 top-32  md:top-44 bg-mountains-herobox mx-auto max-w-100vw md:w-mountains h-mountains" />
       </div>
 
-      <div className=" w-symbol-divisor h-symbol-divisor bg-symbol-divisor mt-32 md:mt-32 mb-10 md:mb-14  mx-auto" />
+      <div className=" w-symbol-divisor h-symbol-divisor bg-symbol-divisor mt-32 md:mt-32 mb-6 md:mb-0  mx-auto" />
 
       <div className="relative flex justify-center">
-        <div className="absolute -inset-y-14 left-14 h-brush-pink-3 w-brush-pink-3 bg-brush-pink-3" />
-        <div className="absolute -inset-y-52 right-20 h-brush-pink-4 w-brush-pink-4 bg-brush-pink-4" />
         <Infographic />
       </div>
 
