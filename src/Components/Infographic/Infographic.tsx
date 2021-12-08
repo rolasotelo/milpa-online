d003027bb1e237b52a66485fc6bd6e0e62f789f1
Rolando Sotelo <rola@hey.com>
2021-12-07 22:36:36 -0600

    🎀 STYLING: Responsive style for paragraph

--- src/Components/Infographic/Infographic.tsx
+++ src/Components/Infographic/Infographic.tsx
@@ -17,11 +17,11 @@ const Infographic = (props: Props) => {
   const { t, i18n } = useTranslation();
   return (
     <div className="relative flex-col w-screen overflow-x-hidden">
-      <div className="relative w-20.38rem mx-auto max-w-100vw md:w-herobox-web h-herobox-web mt-5 md:mt-herobox-top-web">
+      <div className="relative w-20.38rem mx-auto max-w-100vw md:w-herobox-web md:h-herobox-web mt-5 md:mt-herobox-top-web">
         <div className="absolute -inset-y-14 -left-44 h-brush-pink-3 w-brush-pink-3 bg-brush-pink-3" />
         <div className="absolute -inset-y-52 -right-44 h-brush-pink-4 w-brush-pink-4 bg-brush-pink-4" />
-        <div className="relative w-20.38rem mx-auto max-w-100vw md:w-herobox-web h-herobox-web bg-milpaBlue-default ring-8 ring-inset ring-milpaBlue-dark">
-          <div className="flex flex-row  h-full">
+        <div className="relative w-20.38rem mx-auto max-w-100vw md:w-herobox-web md:h-herobox-web bg-milpaBlue-default ring-8 ring-inset ring-milpaBlue-dark">
+          <div className="flex flex-col md:flex-row  h-full">
             <div className="flex flex-col w-72 items-center m-5">
               <div
                 className="text-mexicanBone w-48 h-60"
@@ -59,8 +59,8 @@ const Infographic = (props: Props) => {
                 ))}
               </div>
             </div>
-            <div className="flex flex-col w-full h-full mt-6 mr-10">
-              <div className="col-span-5 w-full h-32 bg-milpaBeige-default ring-8 ring-inset ring-milpaBeige-dark drop-shadow-2xl">
+            <div className="flex flex-col w-full mt-6 mr-10">
+              <div className="col-span-5 w-full h-56  md:h-32 bg-milpaBeige-default ring-8 ring-inset ring-milpaBeige-dark drop-shadow-2xl">
                 <div className="flex flex-col items-center h-full justify-center text-milpaGreen-default text-center">
                   <p
                     className="max-w-lg"
@@ -87,21 +87,20 @@ const Infographic = (props: Props) => {
                 </div>
               </div>
               <div className="row-span-2 col-span-4">
-                <div className="flex flex-row m-1 pt-3 pl-20 relative">
+                <div className="flex flex-col md:flex-row m-1 pt-3 md:pl-20 relative">
                   <div
-                    className="absolute inset-y-32 -left-28 m-1 text-milpaBeige-default w-80 text-right"
+                    className="md:absolute md:inset-y-32 md:-left-28 m-1 pr-5 text-milpaBeige-default w-80 text-right transform md:-rotate-90"
                     style={{
                       fontWeight: 400,
                       fontFamily: "goodlife-sans-condensed, sans-serif",
                       fontStyle: "normal",
                       fontSize: "3rem",
-                      transform: "rotate(-90deg)",
                       lineHeight: "3rem",
                     }}
                   >
                     {t("welcome.infographic.title")}
                   </div>
-                  <div className="flex flex-row">
+                  <div className="flex flex-col md:flex-row">
                     <div
                       className="m-5 text-milpaBeige-default leading-relaxed text-justify"
                       style={{
@@ -114,7 +113,7 @@ const Infographic = (props: Props) => {
                       {t("welcome.infographic.paragraph1")}
                     </div>
                     <div
-                      className="mt-5 mb-5 ml-5 text-milpaBeige-default leading-relaxed text-justify"
+                      className="mr-5 md:mr-0 mt-5 mb-5 ml-5 text-milpaBeige-default leading-relaxed text-justify"
                       style={{
                         fontWeight: 400,
                         fontFamily: "bookmania, sans-serif",
